34cddac51c86d1e69cdeee23760b10913db57317
Felipe Ríos <farios@uc.cl>
2025-02-14 12:02:57 -0300

    Se agrega dibujado de piezas

---
+++ src/logic.py
@@ -0,0 +1,44 @@
+class ChessPiece:
+    def __init__(self, ptype: str, col: str, row: int, white_player: bool):
+        self.type = ptype
+        self.col = col
+        self.row = row
+        self.white_player = white_player
+        self.on_board = True #Capturada o en el tablero
+    
+    def allowed_movements(self):
+        pass
+
+class ChessBoard:
+    def __init__(self):
+        #Inicialmente el tablero tiene una representación en matriz de 8x8
+        #Esta representación no es óptima para la búsqueda con la lógica -> más adelante se utilizará BitMap
+        #Por ahora esta representación servirá a la UI para dibujar el tablero
+        self.pieces = self._initialize_pieces()
+
+
+    def _initialize_pieces(self):
+        cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        pieces = {}
+        pieces['white king']    = ChessPiece('king', 'E', 1, True)
+        pieces['white queen']   = ChessPiece('queen', 'D', 1, True)
+        pieces['white bishop1'] = ChessPiece('bishop', 'F', 1, True)
+        pieces['white bishop2'] = ChessPiece('bishop', 'C', 1, True)
+        pieces['white knight1'] = ChessPiece('bishop', 'G', 1, True)
+        pieces['white knight2'] = ChessPiece('bishop', 'B', 1, True)
+        pieces['white rook1']   = ChessPiece('bishop', 'H', 1, True)
+        pieces['white rook2']   = ChessPiece('bishop', 'A', 1, True)
+        pieces['white king']    = ChessPiece('king', 'E', 8, False)
+        pieces['black queen']   = ChessPiece('queen', 'D', 8, False)
+        pieces['black bishop1'] = ChessPiece('bishop', 'F', 8, False)
+        pieces['black bishop2'] = ChessPiece('bishop', 'C', 8, False)
+        pieces['black knight1'] = ChessPiece('bishop', 'G', 8, False)
+        pieces['black knight2'] = ChessPiece('bishop', 'B', 8, False)
+        pieces['black rook1']   = ChessPiece('bishop', 'H', 8, False)
+        pieces['black rook2']   = ChessPiece('bishop', 'A', 8, False)
+        
+        for col, i in enumerate(cols):
+            pieces[f'white pawn{i+1}'] = ChessPiece('pawn', col, 2, True)
+            pieces[f'black pawn{i+1}'] = ChessPiece('pawn', col, 7, False)
+
+        return pieces
--- src/ui.py
+++ src/ui.py
@@ -1,21 +1,26 @@
 import tkinter as tk
 import yaml
+from PIL import Image, ImageTk
+from logic import ChessBoard
+import pdb
 
 class ChessUI:
     def __init__(self, master, rotation):
         with open('config/board.yaml', 'r') as file: 
             self.config = yaml.safe_load(file)
+        self.cell_size  = self.config['Size']
 
-        self.master = master
-        self.canvas = tk.Canvas(master, 
-                                width=self.config['Size']['width'], 
-                                height=self.config['Size']['height'])
-        self.cell_size = self.config['Size']['cell_size']
-        self.rotation = rotation #False if White's view, True for Black's view
+        self.master     = master
+        self.rotation   = rotation #False if White's view, True for Black's view
+        self.board      = ChessBoard()
+        self.canvas     = tk.Canvas(master, 
+                                    width=self.cell_size * 8, 
+                                    height=self.cell_size * 8)
         self.canvas.pack()
-        self.draw_board(self.player)
+        self.pieces     = self._load_pieces()
+        self.draw_board(self.rotation)
         self.canvas.bind("<Button-1>", self.on_click)
-        self.selected = None #To store selected box
+        self.selected   = None #To store selected box
 
     def draw_board(self, rotation):
         """
@@ -24,14 +29,46 @@ class ChessUI:
         colors = ["white", "gray"]
         for row in range(8):
             for col in range(8):
-                color = colors[(row + col) % 2]
-                x1 = col * self.cell_size
-                y1 = row * self.cell_size
-                x2 = x1 + self.cell_size
-                y2 = y1 + self.cell_size
+                color   = colors[(row + col) % 2]
+                x1      = col * self.cell_size
+                y1      = row * self.cell_size
+                x2      = x1 + self.cell_size
+                y2      = y1 + self.cell_size
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
 
         #Dibujar las piezas
+        for piece_name in self.board.pieces.keys():
+            piece       = self.board.pieces[piece_name]
+            col, row    = self._translate_position(piece.col, piece.row)
+            color       = 'white' if 'white' in piece_name else 'black'
+            key         = f'{color} {piece.ptype}'
+            self.canvas.create_image(col, row, image=self.pieces[key], anchor='nw')
+
+    def _translate_position(self, logic_col, logic_row):
+        
+        cols_str = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] 
+        if self.rotation: #Sin tener en cuenta la rotación primero
+            cols_str = cols_str[::-1]
+        cols_int = {col:num for col, num in enumerate(cols_str)}
+        
+        col = cols_int[logic_col] * self.cell_size
+        row = (logic_row - 1) * self.cell_size
+        
+        return col, row
+
+    def _load_pieces(self):
+        def load_and_format(path):
+            image = Image.open(path)
+            image = image.resize((self.cell_size, self.cell_size), Image.ANTIALIAS)
+            return ImageTk.PhotoImage(image)
+        
+        images = {}
+        for side in self.config['Pieces paths'].keys():
+            for piece in self.config['Pieces paths'][side].keys():
+                key = f'{side} {piece}'
+                images[key] = load_and_format(self.config['Pieces paths'][side][piece])
+
+        return images
 
     def on_click(self, event):
         # Determina la casilla clickeada y maneja la selección/movimiento
@@ -55,5 +92,5 @@ class ChessUI:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ChessUI(root)
+    app = ChessUI(root, False)
     root.mainloop()            
